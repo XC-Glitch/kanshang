554f43dc163440824bfb1b277e291024cdf12491
XC-Glitch <2028214811@qq.com>
2026-02-27 23:28:35 +0800

    提交

---
+++ tool-data.js
@@ -0,0 +1,42 @@
+const toolList = [
+  {
+    id: 1,
+    name: "图片压缩",
+    desc: "免费在线压缩图片，不损失画质",
+    url: "https://baidu.com",
+    view: 210,
+    like: 47
+  },
+  {
+    id: 2,
+    name: "PDF转换",
+    desc: "PDF转Word、图片、PPT等",
+    url: "https://baidu.com",
+    view: 195,
+    like: 53
+  },
+  {
+    id: 3,
+    name: "在线PS",
+    desc: "网页版Photoshop，无需安装",
+    url: "https://baidu.com",
+    view: 166,
+    like: 39
+  },
+  {
+    id: 4,
+    name: "文字识别",
+    desc: "图片转文字，免费高精度",
+    url: "https://baidu.com",
+    view: 243,
+    like: 61
+  },
+  {
+    id: 5,
+    name: "视频解析",
+    desc: "解析各大平台视频地址",
+    url: "https://baidu.com",
+    view: 310,
+    like: 92
+  }
+];
